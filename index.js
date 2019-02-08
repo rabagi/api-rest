284753fa1355e48830e3403feaf2084e84adb94a
rabagi <programador1@telecomo.com.mx>
2019-02-07 18:16:45 -0600

    postman para testear api rest

--- index.js
+++ index.js
@@ -9,10 +9,31 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hola/:name',(req, res) => {
-	res.send({ message: `Hola ${req.params.name}!` })
-}) 
 
+app.get('/api/producto', (req, res) => {
+	//deprecated
+	//res.send(200, {productos: []})
+
+	///good
+	res.status(200).send({productos: []})
+})
+
+app.get('/api/producto/:id', (req, res) => {
+
+})
+
+app.post('/api/producto', (req, res) => {
+	console.log(req.body)
+	res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+app.put('/api/producto/:id', (req, res) => {
+
+})
+
+app.delete('/api/producto/:id', (req, res) => {
+
+})
 
 app.listen(port, ()=> {
 	console.log(`Api-Res en http://localhost:${port}`)
